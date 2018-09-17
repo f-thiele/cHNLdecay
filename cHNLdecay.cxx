9ca6e411ae4763c15ba23e0b2a845b2b7ec030d3
Fabian A.J. Thiele <fabian.thiele@posteo.de>
2018-09-17 16:09:25 +0200

    Add command line options for main routine and configure only one HNL now

--- cHNLdecay.cxx
+++ cHNLdecay.cxx
@@ -7,6 +7,7 @@
 #include "TLegend.h"
 #include "TMath.h"
 #include "TF1.h"
+#include "TObjString.h"
 
 #include "HNL.h"
 #include "Lepton.h"
@@ -20,13 +21,21 @@
 #include "partialWidths.h"
 #include "plots.h"
 #include "Logger.h"
+#include <stdlib.h>
+#include <getopt.h>
 
 Level gLOGLEVEL;
-
-int main() {
-  gLOGLEVEL = Level::INFO;
-
+int main (int argc, char **argv)
+{
   auto cfg = std::make_shared<Config>(); // set BITS and initialize constants
+
+  // initialize the three variables that define our HNL
+  Int_t HNLmass = 0;
+  Double_t angle = 0;
+  std::vector<Lepton> mixes_with;
+
+  // sensible default for loglevel
+  gLOGLEVEL = Level::INFO;
 
   Double_t muonMass = 105.6583715; // MeV
   Double_t electronMass = 0.5109989461; // MeV
@@ -66,14 +75,110 @@ int main() {
 
   std::vector<Meson> mesons = {pi, K, D, Ds, B, Bc, pi0, eta, etaprime, etac, rho, Dstar, Dstars, rho0, omega, phi, jpsi};
 
-  HNL N = HNL("20G", 20000, 3.55e-6, {el, mu, tau}); // 20 GeV but notated in MeV, allow mixing with all flavours (only possible implemtation currently is 1:1:1)
+  while (1)
+    {
+      int c;
+      static struct option long_options[] =
+        {
+          /* These options set a flag. */
+          {"loglevel",     required_argument, 0, 'l'},
+          {"generations",  required_argument, 0, 'g'},
+          {"angle",        required_argument, 0, 'u'},
+          {"mass",         required_argument, 0, 'm'},
+          {0, 0, 0, 0}
+        };
+
+      /* getopt_long stores the option index here. */
+      int option_index = 0;
+
+      c = getopt_long (argc, argv, "v:l:u:m:",
+                       long_options, &option_index);
+
+      /* Detect the end of the options. */
+      if (c == -1)
+        break;
+
+      switch (c)
+        {
+        case 0:
+          /* If this option set a flag, do nothing else now. */
+          if (long_options[option_index].flag != 0)
+            break;
+          printf ("option %s", long_options[option_index].name);
+          if (optarg)
+            printf (" with arg %s", optarg);
+          printf ("\n");
+          break;
+
+        case 'l':
+          {
+            TString x = TString(optarg);
+            if(x.CompareTo("debug")==0) {
+              gLOGLEVEL = Level::DEBUG;
+
+            } else if (x.CompareTo("warning")==0) {
+              gLOGLEVEL = Level::WARNING;
+
+            } else if (x.CompareTo("error")==0) {
+              gLOGLEVEL = Level::ERROR;
+
+            } else {
+              gLOGLEVEL = Level::INFO;
+            }
+          }
+
+          break;
+
+        case 'g':
+          {
+            TString x = TString(optarg);
+            TObjArray *tx = x.Tokenize(",");
+
+            for (Int_t i = 0; i < tx->GetEntries(); i++) {
+              TString iter_str = ((TObjString*)tx->At(i))->CopyString();
+              for(auto l : all_leptons) {
+                if(iter_str.CompareTo(l.getName())==0) mixes_with.emplace_back(l);
+              }
+            }
+          }
+
+          break;
+
+        case 'u':
+          angle = std::atof(optarg);
+          break;
+
+        case 'm':
+          HNLmass = std::atoi(optarg);
+          break;
+
+        case '?':
+          /* getopt_long already printed an error message. */
+          break;
+
+        default:
+          abort ();
+        }
+    }
+
+  /* Print any remaining command line arguments (not options). */
+  if (optind < argc)
+    {
+      printf ("non-option ARGV-elements: ");
+      while (optind < argc)
+        printf ("%s ", argv[optind++]);
+      putchar ('\n');
+    }
+
+
+  for(auto l : mixes_with) {
+    LOG_DEBUG("HNL mixes with " << l.getName());
+  }
+  // this is the HNL we configure via command line options. If it mixes with multiple generations it can only
+  // assume mixing angle ratios 1:1:..:1
+  HNL N = HNL("HNL", HNLmass, angle, mixes_with);
 
   Double_t Gamma = N.getTotalWidth(cfg, all_leptons, mesons);
-  LOG_INFO("mass=" << N.getMass()/1000. << " GeV, " <<
-           "ctau=" << gamma2ctau(cfg, Gamma) << " mm");
-
-  N = HNL("20G", 20000, 3.55e-6, mu); // 20 GeV but notated in MeV, allow mixing with only muon flavour
-  Gamma = N.getTotalWidth(cfg, all_leptons, mesons);
   LOG_INFO("mass=" << N.getMass()/1000. << " GeV, " <<
            "ctau=" << gamma2ctau(cfg, Gamma) << " mm");
 
@@ -83,9 +188,8 @@ int main() {
       std::cout << "\\Gamma(" << it->first << ") = \\SI{" << it->second << "}{}\\\\\n";
   }
 
-  LOG_INFO("Searching now for U2 for 0.1 mm ctau... This might take a while!");
-  Double_t foundU2 = ctauToU2(cfg, 0.1, all_leptons, mesons, N);
-  LOG_INFO(std::endl << "FOUND U2: " << foundU2);
+
+  plot_br_low(cfg, all_leptons, mesons, N, "BR_low.pdf", 50, 1000, 1);
 
   std::vector<Meson> vector_charged;
   for(auto m : mesons) {
@@ -102,15 +206,26 @@ int main() {
   LOG_INFO("QCD coupling at Z mass: " << qcd_coupling(91.2));
   delete f;
 
-  N.setMass(3000);
-  LOG_INFO("pw: " << N.getPartialWidth(cfg, mu, rho));
-
-  plot_meson_pw(cfg, mu, vector_charged, N, "mesons_vector_charged.pdf", 500, 5000);
   plot_I();
   plot_br(cfg, all_leptons, mesons, N, "BR.pdf", 1000, 5000);
 
   plot_qcd_correction("qcd_corr.pdf");
   plot_qcd_coupling("qcd_coupl.pdf");
+
+  bool isMajorana = N.isMajorana();
+  N.setMajorana(false); // disable majorana here because we want to compare the pw for one charge configuration only
+  plot_meson_pw(cfg, mu, vector_charged, N, "mesons_vector_charged.pdf", 500, 5000);
+  N.setMajorana(isMajorana); // restore majorana value
+
+  LOG_INFO("Searching now for U2 for 0.1 mm ctau... This might take a while!");
+  Double_t foundU2 = ctauToU2(cfg, 0.1, all_leptons, mesons, N);
+  LOG_INFO(std::endl << "FOUND U2: " << foundU2);
+
+  // EXAMPLE for further checks and configurations
+  // ----------
+  // N.setMass(3000);
+  // LOG_INFO("pw: " << N.getPartialWidth(cfg, mu, rho));
+
 
   return EXIT_SUCCESS;
 }
