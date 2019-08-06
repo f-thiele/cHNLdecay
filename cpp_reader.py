87dd9a26bd43f6f05dfe00364a4337ede6e18340
bouchiba <bouchiba@tsf-452-wpa-3-060.epfl.ch>
2019-08-06 15:32:43 +0200

    minor adds

--- cpp_reader.py
+++ cpp_reader.py
@@ -1,9 +1,8 @@
 from subprocess import Popen, PIPE
 
-output = Popen(['./cHNLdecay', '--generations', '13', '--mass', '20000', '--angle', '3.5e-6'], stdout=PIPE)
+output = Popen(['./cHNLdecay', '--mainmode', '1', '--BmesonID', '521', '--generations', '13', '--mass', '3500', '--angle', '3.5e-6'], stdout=PIPE)
 out = output.stdout.read()
 
-print "out: ", out
 out_pw = out.split()[0];
 
 print out_pw;
